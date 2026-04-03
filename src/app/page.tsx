e6aefa495934299b9e7d20643ca1058fd1b3c925
Paras60 <choudharyparas61@gmail.com>
2026-04-03 01:22:47 -0400

    Removed modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,11 @@ import AgentSection from "@/components/home/AgentSection";
 import TestimonialsSection from "@/components/home/TestimonialsSection";
 import CTASection from "@/components/home/CTASection";
 import YouTubeSection from "@/components/home/YouTubeSection";
-import LeadCaptureModal from "@/components/home/LeadCaptureModal";
 import NeighbourhoodSection from "@/components/home/NeighbourhoodSection";
 
 export default function HomePage() {
   return (
     <>
-      <LeadCaptureModal />
       <HeroSection />
       <StatsBar />
       <FeaturedListings />
